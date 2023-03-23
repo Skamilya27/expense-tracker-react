e3a4ac03e03efb833c990c71173abc3e2a87ea89
Suman <sumankamilya27@gmail.com>
2023-03-23 19:51:27 +0530

    task 10 edit and delete

--- src/components/Pages/Expenses/ExpenseContainer.jsx
+++ src/components/Pages/Expenses/ExpenseContainer.jsx
@@ -1,20 +1,67 @@
-import React, { useContext } from 'react'
-import { expenseContext } from '../../../context/ExpenseContext'
+// import React, { useContext, useState } from 'react'
+// import { expenseContext } from '../../../context/ExpenseContext'
+// import EditForm from './EditForm';
 
-function ExpenseContainer() {
+// function ExpenseContainer() {
 
-    const { expenses } = useContext(expenseContext)
-  return (
-    <>
-     {expenses.map((e, i) => 
-        <div className=' d-flex justify-content-around m-2 p-2 shadow' key={i}>
+//     const { expenses, deleteExpense } = useContext(expenseContext)
+//     const [editShow, setEditShow] = useState(false);
+//   return (
+//     <>
+//      {expenses.map((e, i) => 
+//         <div className=' d-flex justify-content-around m-2 p-2 shadow' key={i}>
+//             <p>Amount - {e.Amount}</p>
+//             <p>Description - {e.Description}</p>
+//             <p>Category - {e.Category}</p>
+
+//             <div className='d-flex gap-2'>
+//                 <EditForm item={e} />
+//                 <button
+//                     onClick={() => deleteExpense(e.id)}
+//                     className='btn btn-danger'
+//                 >
+//                     Delete
+//                 </button>
+//             </div>
+//         </div>
+//      )} 
+//     </>
+//   )
+// }
+
+// export default ExpenseContainer
+
+import React, { useContext, useEffect, useState } from 'react'
+import { expenseContext } from '../../../context/ExpenseContext';
+import EditForm from './EditForm';
+
+const ExpenseContainer = () => {
+    const { expenses, deleteExpense } = useContext(expenseContext);
+    const [editShow,setEditShow] = useState(false)
+    return (
+      <div>
+        {expenses.map((e, i) => (
+          
+        <div
+            className=" d-flex justify-content-around m-2 p-2 shadow"
+            key={i}
+          >
             <p>Amount - {e.Amount}</p>
             <p>Description - {e.Description}</p>
             <p>Category - {e.Category}</p>
-        </div>
-     )} 
-    </>
-  )
-}
+            <div className=" d-flex gap-2">
+             <EditForm item={e} />
+              <button
+                onClick={() => deleteExpense(e.id)}
+                className=" btn btn-danger"
+              >
+                DELETE
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+};
 
 export default ExpenseContainer
--- src/context/ExpenseContext.jsx
+++ src/context/ExpenseContext.jsx
@@ -1,38 +1,56 @@
-import axios from "axios";
-import React, { createContext, useEffect, useState } from "react";
+import axios from 'axios'
+import React, { createContext, useEffect, useState } from 'react'
 
 export const expenseContext = createContext()
 
-const ExpenseProvider = ({children}) => {
-    const [expenses, setExpenses] = useState([]);
-    useEffect(() => {
-        getExpenses()
-    }, []);
+const ExpenseProvider = ({ children }) => {
+  const [expenses, setExpenses] = useState([]);
+  useEffect(() => {
+  getExpenses()
+  }, []);
 
-    async function getExpenses() {
+      async function getExpenses() {
         const res = await axios.get(
-            "https://expense-tracker-react-14d46-default-rtdb.firebaseio.com/expenses.json"
+          "https://expense-tracker-react-14d46-default-rtdb.firebaseio.com/expenses.json"
         );
-
         setExpenses([])
-            for(let key in res.data) {
-                setExpenses((prev) => [...prev, res.data[key]])
-            }
-    }
+          for (let key in res.data) {
+            setExpenses((prev) => [...prev, { id : key,...res.data[key] }])
+        }
 
-    async function postExpense(item) {
-        const res = await axios.post(
-            "https://expense-tracker-react-14d46-default-rtdb.firebaseio.com/expenses.json", item
-        );
-        console.log(res)
+    }
+  
+  async function deleteExpense(id) {
+    await axios.delete(
+      `https://expense-tracker-react-14d46-default-rtdb.firebaseio.com/expenses/${id}.json`
+    );
+    getExpenses()
+  }
+  
+    async function editExpense(id,item) {
+      const res = await axios.put(
+        `https://expense-tracker-react-14d46-default-rtdb.firebaseio.com/expenses/${id}.json`,
+        item
+      );
+      console.log(res)
+      getExpenses();
+  }
+  
+     async function postExpense(item) {
+      const res =  await axios.post(
+         "https://expense-tracker-react-14d46-default-rtdb.firebaseio.com/expenses.json"
+       ,item);
+      // console.log(res)
         getExpenses()
-    }
+     }
 
-    return (
-        <expenseContext.Provider value={{ expenses, postExpense }}>
-            {children}
-        </expenseContext.Provider>
-    )
+  return (
+    <expenseContext.Provider
+      value={{ deleteExpense, expenses, postExpense, editExpense }}
+    >
+      {children}
+    </expenseContext.Provider>
+  );
 };
 
 export default ExpenseProvider;
